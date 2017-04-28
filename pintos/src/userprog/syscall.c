3dead742db668226a6aee90c473822274c4799af
AbnerZheng <abnerzheng@gmail.com>
2017-04-28 08:57:56 +0000

    改成函数向量的方法,统一检查参数

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -9,71 +9,72 @@
 #include "pagedir.h"
 
 static void syscall_handler (struct intr_frame *);
-void syswrite(struct intr_frame *f UNUSED);
-void syscreate(struct intr_frame *f UNUSED);
+
+static int syswrite (int fd, const void *buffer, unsigned size);
+
+static int syscreate (const char *file, unsigned initial_size);
+
+static int sysexit (int status);
+
+typedef int (*handler) (uint32_t, uint32_t, uint32_t);
+
+static handler syscall_vec[128];
 
 void
 syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  syscall_vec[SYS_EXIT]   = (handler) sysexit;
+  syscall_vec[SYS_CREATE] = (handler) syscreate;
+  syscall_vec[SYS_WRITE]  = (handler) syswrite;
 }
 
-void fail_exit(){
-  printf("%s: exit(%d)\n", &thread_current ()->name, -1);
-  thread_exit ();
-}
-
-void validate_addr(uint32_t* addr){
-  if(!is_user_vaddr (addr)){
-    fail_exit ();
+void validate_addr (uint32_t *addr, uint32_t cs)
+{
+  if (!is_user_vaddr (addr)) {
+    sysexit (-1);
   }
 }
 
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
-  uint32_t* args = ((uint32_t*) f->esp);
+  uint32_t *args = ((uint32_t *) f->esp);
 
-  if(args < f->eip){
-    fail_exit ();
-  }
-  switch (args[0]){
-    case SYS_EXIT:
-      f->eax = args[1];
-      validate_addr (args[1]);
-      printf("%s: exit(%d)\n", &thread_current ()->name, args[1]);
-      thread_exit();
-      break;
-    case SYS_WRITE:
-      // 首先获取fid
-      syswrite(f);
-      break;
-    case SYS_CREATE:
-      syscreate(f);
-      break;
+  validate_addr (args[0], 0);
+
+  if (args[0] < SYS_EXIT || args[0] > SYS_INUMBER) {
+    sysexit (-1);
   }
 
+  handler h = syscall_vec[args[0]];
+
+  validate_addr (args[1], 0);
+  validate_addr (args[2], 0);
+  validate_addr (args[3], 0);
+
+  int ret = h (args[1], args[2], args[3]);
+  f->eax = ret;
+}
+
+static int
+sysexit (int status)
+{
+  printf ("%s: exit(%d)\n", &thread_current ()->name, status);
+  thread_exit ();
+}
+
+static int
+syscreate (const char *file, unsigned initial_size)
+{
+  if (strlen (file) == 0) {
+    return 0;
+  }
 }
 
 
-void syscreate(struct intr_frame *f UNUSED){
-  uint32_t *args = ((uint32_t*) f->esp);
-  char* file = args[1];
-  if(!file){
-    fail_exit ();
-  }
-
-  if(strlen (file) == 0){
-     f->eax = 0;
-  }
-}
-
-
-void syswrite(struct intr_frame *f UNUSED){
-  uint32_t* args = ((uint32_t*) f->esp);
-  int fid = args[1];
-  char* buffer = args[2]; // 这时还只是用户地址,要转换为系统地址
-  char* phy_buffer =  pagedir_get_page (thread_current ()->pagedir, buffer);
-  int size = args[3];
+static int syswrite (int fd, const void *buffer, unsigned size)
+{
+  char *phy_buffer = pagedir_get_page (thread_current ()->pagedir, buffer);
   putbuf (phy_buffer, size);
 }
